74cff032763b02142ca969237eb86f3ca3c9d91b
@gi0cann <curakill@gmail.com>
2016-10-07 15:41:34 -0400

    add git clone for scorebot to build script.

--- scorebot-setup.sh
+++ scorebot-setup.sh
@@ -2,12 +2,13 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password abc
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password abcd1234'
 
 apt-get update
-apt-get install -y python-dnspython python-rrdtool apache2 php5 php5-mysql python-pymongo python-dns python-mysqldb wget curl git mongodb mysql-server mysql-client
-curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
+apt-get upgrade -y
+apt-get install -y python-dnspython python-rrdtool apache2 php5 php5-mysql python-pymongo python-dns python-mysqldb wget curl git mongodb mysql-server mysql-client pip2
 pip install jaraco.modb bottle paste requests
 
 cp -rf /home/vagrant/deps/sts/ /var/www/html
+
+git clone https://github.com/dichotomy/scorebot /home/vagrant/scorebot
 
 mysql -u root -pabcd1234 < /home/vagrant/deps/db.sql
 mysql -u scorebot -ppassword sts < /home/vagrant/deps/sts/config/sts.sql
