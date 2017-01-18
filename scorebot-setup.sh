e38706f282315779c584e4f8a777b291df8e8b03
@gi0cann <curakill@gmail.com>
2017-01-17 20:38:01 -0500

    Fixes use of directory deps instead of dev. Fixes pip installation

--- scorebot-setup.sh
+++ scorebot-setup.sh
@@ -3,7 +3,7 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y python-dnspython python-rrdtool apache2 php5 php5-mysql python-pymongo python-dns python-mysqldb wget curl git mongodb mysql-server mysql-client pip2
+apt-get install -y python-dnspython python-rrdtool apache2 php5 php5-mysql python-pymongo python-dns python-mysqldb wget curl git mongodb mysql-server mysql-client python-pip
 pip install jaraco.modb bottle paste requests
 
 cp -rf /home/vagrant/dev/sts/ /var/www/html
@@ -11,6 +11,6 @@ cp -rf /home/vagrant/dev/sts/ /var/www/html
 git clone https://github.com/dichotomy/scorebot /home/vagrant/dev/scorebot
 
 mysql -u root -pabcd1234 < /home/vagrant/dev/db.sql
-mysql -u scorebot -ppassword sts < /home/vagrant/deps/sts/config/sts.sql
+mysql -u scorebot -ppassword sts < /home/vagrant/dev/sts/config/sts.sql
 
 service apache2 restart
